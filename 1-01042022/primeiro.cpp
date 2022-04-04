c379f8acc92dc40dc5e656484f5852ba0bb4cbb1
Jurandir <jurandirfnvarcal@gmail.com>
2022-04-04 08:57:22 -0300

    Estudo C++

---
+++ 1-01042022/primeiro.cpp
@@ -0,0 +1,18 @@
+/*Programa para exibir uma mensagem. 
+O código foi escrito em C++
+
+Importação da biblioteca de entrada e saída 
+de dados*/
+
+#include <iostream>
+
+int main(){
+
+//saída padrão de dados
+//cout (console out => Saída na tela de console (terminal))
+std::cout << "Bem vindo ao C++\n";
+
+
+return 0;
+
+}
