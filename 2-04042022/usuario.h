db09277ad84038eae2277f6a69743821426b0287
Jurandir <jurandirfnvarcal@gmail.com>
2022-04-04 11:08:24 -0300

    Criação da classe Usuario

---
+++ 2-04042022/usuario.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+
+class Usuario {
+    
+    public:
+        string nome;
+        string senha;
+        string email;
+        string nivel;
+
+
+
+
+
+};
